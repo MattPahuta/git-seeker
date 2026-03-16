bee38063e99a7f505f75097bb20138d7152a580c
Matt Pahuta <mhpahuta@gmail.com>
2026-03-16 16:30:53 -0700

    Add initial component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-    <div className="mx-auto p-6 max-w-2xl">
+    <div className="mx-auto max-w-3xl">
       <h1 className="text-3xl font-bold text-emerald-500">GitSeeker</h1>
     </div>
   )
